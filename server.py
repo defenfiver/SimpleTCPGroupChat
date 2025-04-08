282291d8193a3d9fe44463a514373aecd8522077
connor-valley1 <valleyco@mail.gvsu.edu>
2025-04-08 14:58:19 -0400

    take username and store clients array

--- server.py
+++ server.py
@@ -1,20 +1,33 @@
 from threading import Thread
 from socket import socket, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR
 
+clients = []
 
 def handleClient(sock):
     # Handle communication with one client
-    socket.send("Hi")
+    sock.send(b"Enter your name")
+    name = sock.recv(1024)  # Receive the name from the client
+    clients.append(name)
+    name = str(name)
+    sock.send(b"Client connected")
+    print(f"Client connected: {name}")
+    print(f"All connected clients: {clients}")
     # Remember to close the socket when done
     sock.close()
 
 server_socket = socket(AF_INET, SOCK_STREAM)
-server_socket.bind(("", 5000))
+server_socket.bind(("", 80))
 server_socket.listen(4)
-while True:
-    connection_socket, _ = server_socket.accept()
-    t = Thread(target = handleClient, args=(connection_socket,))
-    t.start()
-    hi = input("type anything to quit")
-    if not hi:
-        server_socket.close()
+
+try:
+  while True:
+      connection_socket, _ = server_socket.accept()
+      t = Thread(target = handleClient, args=(connection_socket,))
+      t.start()
+
+except KeyboardInterrupt:
+  print("Shutting Down Server")
+
+finally:
+   server_socket.close()
+    
