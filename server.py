e430c3a85d42b2ee30648110c57373ca84a1bbee
defenfiver <defenfiver45@gmail.com>
2025-04-08 19:11:32 -0400

    Completed server

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+from ctypes import WinError
 from threading import Thread
 from socket import socket, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR
 
@@ -5,30 +6,50 @@ from socket import socket, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR
 def handleClient(sock):
     # Handle communication with one client
     sock.send("Enter your name".encode())
-    name = sock.recv(1024).decode()  # Receive the name from the client
+    try: 
+        name = sock.recv(1024).decode()  # Receive the name from the client
+    except ConnectionResetError:
+        sock.close()
+        print("[Server Error]: Client closed before name was entered")
+        return
     clients[sock] = name
     name = str(name)
-    sock.send("Client connected".encode())
-    print(f"Client connected: {name}")
-    names = ""
-    for x in clients:
-        names = f'{names}, {clients[x]}'
-    print(f"All connected clients: {names}")
+    sendClients(f'[Server]: {name} has connected'.encode())
+    data = f'[Server]: Currently Connected: {getAllClients()}'
+    sock.send(data.encode())
+    print(f'{data} (Only sent to {name})')
     while True:
-        data = sock.recv(1024).decode()
-        data = f'{name}: {data}'
-        sendClients(data)
+        try:
+            data = sock.recv(1024).decode()
+        except ConnectionResetError:
+            clients.pop(sock)
+            sendClients(f'[Server]: {name} has disconnected'.encode())
+            sendClients(f"[Server]: Users remaining: {getAllClients()}".encode())
+            sock.close()
+            return
+        sendClients(f'[{name}]: {data}'.encode())
     # Remember to close the socket when done
     sock.close()
 
+def getAllClients():
+    if not clients.keys():
+        return ""
+    nameList = []
+    for x in clients:
+        nameList.append(clients[x])
+    names = nameList.pop(0)
+    for x in nameList:
+        names += f', {x}'
+    return names
+
 def sendClients(message):
     for x in clients:
-        x.send(message.encode())
+        x.send(message)
+    print(f'{message.decode()}')
 
 def accepts():
     while True:
         connection_socket, tmp = server_socket.accept()
-        print("User connected")
         Thread(target=handleClient, args=(connection_socket,)).start()
         
 
